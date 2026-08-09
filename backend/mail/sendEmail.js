38a00bdb1d1f5a02e09ba767ca59c62531704d21
casmartstaycation <markryantamayo@gmail.com>
2026-08-10 06:23:27 +0800

    Add SMTP timeouts for reliable payment uploads

--- backend/mail/sendEmail.js
+++ backend/mail/sendEmail.js
@@ -1,33 +1,25 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
     service: "gmail",
-
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
-    }
-
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000
 });
 
 async function sendEmail(to, subject, html) {
+    if (!to) return;
 
     await transporter.sendMail({
-
         from: `"CA Smart Staycation" <${process.env.EMAIL_USER}>`,
-
         to,
-
         subject,
-
         html
-
     });
-
 }
 
 module.exports = sendEmail;
